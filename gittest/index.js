b7d3a7f84ae67dcff910213dd5544e5e59f542a6
sakurakotubaki <gitonly543@gmail.com>
2025-02-13 08:51:08 +0900

    creaet index.js

---
+++ gittest/index.js
@@ -0,0 +1,10 @@
+// array: number[] = [1, 2, 3, 4, 5]
+let array = [1, 2, 3, 4, 5];
+
+// add value to array
+array.push(6);
+console.log(array);
+
+// remove value from array
+array.pop();
+console.log(array);
